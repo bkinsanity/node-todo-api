a418c93d6445cecb977ded394375d6b73d07bd8a
bkinsanity <bkinsanity@gmail.com>
2017-06-26 23:06:09 -0700

    Add Get /todos route

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {User} = require('./models/user');
 var app = express();
 
 app.use(bodyParser.json());
+
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -19,6 +20,14 @@ app.post('/todos', (req, res) => {
         res.status(400).send(err);
     });
 })
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
